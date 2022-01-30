c7c0ed6df9695e1cc3b2280081cce09c0981df0e
Elvis Carrasco <elvissamir18@gmail.com>
2022-01-30 15:25:25 -0400

    Added content for About Me Section.

--- public/js/app.js
+++ public/js/app.js
@@ -38190,7 +38190,9 @@ var _hoisted_52 = /*#__PURE__*/(0,vue__WEBPACK_IMPORTED_MODULE_0__.createElement
   "class": "title-line w-6/12"
 })])]), /*#__PURE__*/(0,vue__WEBPACK_IMPORTED_MODULE_0__.createElementVNode)("div", {
   "class": "section-content"
-})])], -1
+}, [/*#__PURE__*/(0,vue__WEBPACK_IMPORTED_MODULE_0__.createElementVNode)("p", {
+  "class": "about-text"
+}, " I'm a passionate software developer and systems engineering student (I'll graduate this year, 2022). My goal is to help you design and develop quality systems that solve real problems and make a difference. In my opinion, good practices and testing are an integral component of the software development process, no matter how big or small the project is. I enjoy learning new things, working out, listening to music, and cooking. Alright, I look forward to hearing from you and working together. ")])])], -1
 /* HOISTED */
 );
 
--- resources/js/Pages/Home/Index.vue
+++ resources/js/Pages/Home/Index.vue
@@ -178,7 +178,13 @@
                         </div>
                     </div> 
 
-                    <div class="section-content"></div>
+                    <div class="section-content">
+                        <p class="about-text">
+                            I'm a passionate software developer and systems engineering student (I'll graduate this year, 2022). My goal is to help you design and develop quality systems that solve real problems and make a difference. In my opinion, good practices and testing are an integral component of the software development process, no matter how big or small the project is. 
+                            I enjoy learning new things, working out, listening to music, and cooking. 
+                            Alright, I look forward to hearing from you and working together.
+                        </p>
+                    </div>
                 </div>
             </div>
 
