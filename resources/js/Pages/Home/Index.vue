f173bd8cf39b2c6d4570d1ebd1743f03eaaf525e
Elvis Carrasco <elvissamir18@gmail.com>
2022-05-05 17:52:35 -0400

    Add Scweb project

--- resources/js/Pages/Home/Index.vue
+++ resources/js/Pages/Home/Index.vue
@@ -310,6 +310,24 @@ export default {
                 demoUrl:  "https://rvmovies.netlify.app/" ,
                 image: '/images/vmoviesPics/vmovie.gif',
                 githubUrl: 'https://github.com/Elvissamir/Fullrvmovies'
+            },
+             {
+                code: 'Front Store',
+                title: 'React Front Store', 
+                description: 'Front Store developed using react and redux',
+                programmingL: 'node',
+                stack: [
+                    '/images/BrandLogos/javascript.png',
+                    '/images/BrandLogos/node.png', 
+                    '/images/BrandLogos/express.png', 
+                    '/images/BrandLogos/mongo.png',
+                    '/images/BrandLogos/react.png',
+                    '/images/BrandLogos/css.png'
+                ],
+                devices: ['desktop'],
+                language: 'English',
+                image: '/images/scwebPics/scweb.gif',
+                githubUrl: 'https://github.com/Elvissamir/Scweb'
             }
         ]);
 
